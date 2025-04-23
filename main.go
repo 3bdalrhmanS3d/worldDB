818ac1933cfe63604b62a7a9e1efdf4e845c97f2
3bdalrhmanS3d <abdalrhmansaad24@gmail.com>
2025-04-23 14:23:40 +0200

    for test2

--- main.go
+++ main.go
@@ -399,7 +399,6 @@ func selectQueryManual(db *sql.DB) {
 }
 
 // INSERT INTO city (Name, CountryCode, District, Population) VALUES ('NewCity', 'USA', 'TestState', 12345);
-
 func insertQueryManual(db *sql.DB) {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter INSERT query: ")
